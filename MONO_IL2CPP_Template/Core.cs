a00d023cf2a910860d3ec023f60dc1b2c2021eeb
weedeej <work.deeej@gmail.com>
2025-05-19 08:49:21 +0800

    Add project files.

---
+++ MONO_IL2CPP_Template/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(MONO_IL2CPP_Template.Core), "MONO_IL2CPP_Template", "1.0.0", "Dixie", null)] // Change this
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace MONO_IL2CPP_Template
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
---
+++ MONO_IL2CPP_Template/TemplateUtils/AssetBundleUtils.cs
@@ -0,0 +1,97 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace MONO_IL2CPP_Template.TemplateUtils
+{
+    public static class AssetBundleUtils
+    {
+        static Core mod = MelonAssembly.FindMelonInstance<Core>();
+        static MelonAssembly melonAssembly = mod.MelonAssembly;
+#if IL2CPP
+        public static Il2CppAssetBundle LoadAssetBundle(string bundleFileName)
+#elif MONO
+        public static AssetBundle LoadAssetBundle(string bundleFileName)
+#endif
+        {
+#if IL2CPP
+            try
+            {
+                Stream bundleStream = melonAssembly.Assembly.GetManifestResourceStream($"{typeof(Core).Namespace}.Assets.launderers.assetbundle");
+                if (bundleStream == null)
+                {
+                    mod.Unregister($"AssetBundle stream not found");
+                    return null;
+                }
+                byte[] bundleData;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bundleStream.CopyTo(ms);
+                    bundleData = ms.ToArray();
+                }
+                Il2CppSystem.IO.Stream stream = new Il2CppSystem.IO.MemoryStream(bundleData);
+                return Il2CppAssetBundleManager.LoadFromStream(stream);
+            }
+            catch (Exception e)
+            {
+                mod.Unregister($"Failed to load AssetBundle. Please report to dev: {e}");
+                return null;
+            }
+#elif MONO
+            try
+            {
+                var stream = melonAssembly.Assembly.GetManifestResourceStream($"{typeof(Core).Namespace}.Assets.launderers.assetbundle");
+
+                if (stream == null)
+                {
+
+                    mod.Unregister($"AssetBundle stream not found");
+                    return null;
+                }
+                return AssetBundle.LoadFromStream(stream);
+            }
+            catch (Exception e)
+            {
+                mod.Unregister($"Failed to load AssetBundle. Please report to dev: {e}");
+                return null;
+            }
+#endif
+        }
+
+        public static
+#if IL2CPP
+            Il2CppAssetBundle
+#elif MONO
+            AssetBundle
+#endif
+            GetLoadedAssetBundle(string asset_name_flag)
+        {
+#if IL2CPP
+
+            Il2CppAssetBundle[] loadedBundles = Il2CppAssetBundleManager.GetAllLoadedAssetBundles();
+#elif MONO
+            AssetBundle[] loadedBundles = AssetBundle.GetAllLoadedAssetBundles().ToArray();
+#endif
+            if (loadedBundles.Length == 1) return loadedBundles[0];
+            try
+            {
+                foreach (var bundle in loadedBundles)
+                {
+                    if (bundle.Contains(asset_name_flag)) return bundle;
+                }
+                throw new Exception($"Asset '{asset_name_flag}' not found in {loadedBundles.Length} bundle(s)");
+            }
+            catch (Exception e)
+            {
+                mod.Unregister($"Failed to get loaded AssetBundle. Please report to dev: {e}");
+                return null;
+            }
+
+        }
+
+        public static GameObject LoadAssetFromBundle(string asset_name)
+        {
+            var bundle = GetLoadedAssetBundle(asset_name);
+            return bundle.LoadAsset<GameObject>(asset_name);
+        }
+    }
+}
